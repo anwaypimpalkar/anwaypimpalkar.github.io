8892135274cd55d92486965f99b95781b3299b82
anwaypimpalkar <anway.pimpalkar@gmail.com>
2024-05-25 17:41:59 -0400

    new site

---
+++ assets/js/scrolling.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 50) {
+    document.getElementById("inner-navbar").style.backgroundColor = "rgba(177,206,232, 0.9)";
+  } else {
+    document.getElementById("inner-navbar").style.backgroundColor = "";
+  }
+}
