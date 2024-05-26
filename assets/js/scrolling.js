030d6924964b6f14a3d08f9d8725270972753134
anwaypimpalkar <anway.pimpalkar@gmail.com>
2024-05-26 14:55:28 -0400

    update

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -7,3 +7,11 @@
 //     document.getElementById("inner-navbar").style.backgroundColor = "";
 //   }
 // }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var last_updated = document.getElementById("last_updated");
+  var d = new Date(document.lastModified);
+  var monthName = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+  var date = monthName[d.getMonth()] + " " + d.getFullYear();
+  last_updated.innerHTML = "homemade " + date;
+});
