bc1f8ab258c56ce638e89a78b11f3f9880ae7e14
anwaypimpalkar <anway.pimpalkar@gmail.com>
2024-12-02 00:00:07 -0500

    2024-12-02 00:00:07

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   var last_updated = document.getElementById("last_updated");
   var d = new Date(document.lastModified);
-  var monthName = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+  var monthName = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
   var date = monthName[d.getMonth()] + " " + d.getFullYear();
-  last_updated.innerHTML = "homemade &nbsp;&nbsp; <i class=\"fa-solid fa-pencil\"></i> " + date;
+  last_updated.innerHTML =
+    'homemade from scratch &nbsp;&nbsp; <i class="fa-solid fa-pencil"></i> ' +
+    date;
 });
 
 function openInNewTab(obj) {
@@ -20,14 +35,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const notyets = document.getElementsByClassName("notyet");
 
   for (let i = 0; i < notyets.length; i++) {
-      let notyet = notyets[i];
+    let notyet = notyets[i];
 
-      notyet.addEventListener("mouseover", () => {
-          notyet.classList.add("no-cursor");
-      });
+    notyet.addEventListener("mouseover", () => {
+      notyet.classList.add("no-cursor");
+    });
 
-      notyet.addEventListener("mouseout", () => {
-          notyet.classList.remove("no-cursor");
-      });
+    notyet.addEventListener("mouseout", () => {
+      notyet.classList.remove("no-cursor");
+    });
   }
 });
